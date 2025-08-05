f942c5d503517890de6861bce4087fe9d3550dc0
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-08-05 16:58:46 +0500

    feat: начальная структура Angular-проекта для Task #6
    - Установлен Tailwind CSS и настроен проект без zone.js
    - Созданы пустые компоненты:
      - login-page
      - presentations-page
      - room-page
    - Созданы сервисы:
      - user.service.ts (сигнальный подход, init из localStorage)
      - presentation.service.ts
      - hub.service.ts (для SignalR)
    - Созданы модели:
      - user.model.ts
      - presentation.model.ts
    - Создан guard: auth.guard.ts (блокирует доступ без user)
    - Созданы файлы: app.routes.ts, app.config.ts
    - Подготовлен environments/environment.ts

---
+++ src/app/pages/presentations/presentations.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentations',
+  imports: [],
+  templateUrl: './presentations.html',
+  styleUrl: './presentations.css'
+})
+export class Presentations {
+
+}
